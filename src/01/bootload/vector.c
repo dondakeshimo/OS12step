78ce4da7ec525e9070f8849c8c08b2383c2a5aed
Shimomura Taku <went.went.takkun135@gmail.com>
2018-05-14 00:31:09 +0900

    [fix]defineの不要な;を消去

--- src/01/bootload/vector.c
+++ src/01/bootload/vector.c
@@ -6,7 +6,7 @@ extern void start(void);
  * 割込みベクタの設定。
  * リンカ・スクリプトの定義により、先頭番地に設置される。
  */
-void (*vectors[]) (void) = {
+void (*vectors[])(void) = {
     start, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
     NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
