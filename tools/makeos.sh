90c231f2aec972008df33d3838a39e2ad5eb951b
Shimomura Taku <went.went.takkun135@gmail.com>
2018-05-20 19:02:00 +0900

    [add]writeもパスを指定できるように変更

--- tools/makeos.sh
+++ tools/makeos.sh
@@ -3,6 +3,6 @@
 case $1 in
   make)  docker run -i -v=/Users/taku/OS12step:/work -w=/work/src/$2 os12step make;;
   image) docker run -i -v=/Users/taku/OS12step:/work -w=/work/src/$2 os12step make image;;
-  write) /Users/taku/OS12step/tools/kz_h8write -3069 -f20 kzload.mot /dev/tty.usbserial-FT1Y6SYF;;
+  write) /Users/taku/OS12step/tools/kz_h8write -3069 -f20 /Users/taku/OS12step/src/$2/kzload.mot /dev/tty.usbserial-FT1Y6SYF;;
   clean) docker run -i -v=/Users/taku/OS12step:/work -w=/work/src/$2 os12step make clean;;
 esac
