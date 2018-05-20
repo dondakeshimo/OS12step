4edf88226e51001c6dbeaf18f27ba2f6efbe5b31
Shimomura Taku <went.went.takkun135@gmail.com>
2018-05-20 18:35:41 +0900

    [add]putxvalの実装

--- src/02/bootload/lib.c
+++ src/02/bootload/lib.c
@@ -87,3 +87,26 @@ int puts(unsigned char *str)
         putc(*(str++));
     return 0;
 }
+
+int putxval(unsigned long value, int cokumn)
+{
+    char buf[9];
+    char *p;
+
+    p = buf + sizeof(buf) - 1;
+    *(p--) = '\0';
+
+    if (!value && column) {
+        column++;
+    }
+
+    while (value || column) {
+        *(p--) = "0123456789abcdef"[value & 0xf];
+        value >>= 4;
+        if (column) column--;
+    }
+
+    puts(p + 1);
+
+    return 0;
+}
--- src/02/bootload/lib.h
+++ src/02/bootload/lib.h
@@ -1,8 +1,6 @@
 #ifndef _LIB_H_INCLUDED_
 #define _LIB_H_INCLUDED_
 
-int putc(unsigned char c);
-int puts(unsigned char *str);
 void *memset(void *b, int c, long len);
 void *memcpy(void *dst, const void *src, long len);
 int memcmp(const void *b1, const void *b2, long len);
@@ -10,5 +8,8 @@ int strlen(const char *s);
 char *strcpy(char *dst, const char *src);
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, int len);
+int putc(unsigned char c);
+int puts(unsigned char *str);
+int putxval(unsigned long value, int column); /* 数値の16進数表示 */
 
 #endif
