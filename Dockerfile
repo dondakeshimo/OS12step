92916ce2e9ae482b8c7d8cd80f34971bd8225256
dondakeshimo <went.went.takkun135@gmail.com>
2018-06-17 18:09:30 +0900

    [fix]makeosを修正、dockerfileの不要なコメントを削除

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,6 @@ RUN apt-get install -y build-essential
 RUN apt-get install -y wget
 RUN apt-get install -y unzip
 
-# RUN wget http://kozos.jp/books/makeos/gcc-3.4.6.tar.gz && wget http://kozos.jp/books/makeos/binutils-2.19.1.tar.gz && wget "https://ja.osdn.net/frs/redir.php?m=ymu&f=kz-h8write%2F57645%2Fkz_h8write-v0.2.1.zip"
-# RUN tar xvzf gcc-3.4.6.tar.gz && tar xvzf binutils-2.19.1.tar.gz
-# RUN mv 'redir.php?m=iij&f=kz-h8write%2F57645%2Fkz_h8write-v0.2.1.zip' kz_h8write.zip
-# RUN unzip kz_h8write.zip
-
 RUN cd /OS12step/binutils-2.19.1 ; ./configure --target=h8300-elf --disable-nls --disable-werror ; make ; make install
 
 RUN sed -i -e 's/redir_handle = open (redir, O_WRONLY | O_TRUNC | O_CREAT)/redir_handle = open (redir, O_WRONLY | O_TRUNC | O_CREAT, 0755)/' /OS12step/gcc-3.4.6/gcc/collect2.c
--- tools/makeos.sh
+++ tools/makeos.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+WORKDIR="$HOME/src/github.com/dondakeshimo/OS12step"
+USBDEV="/dev/tty.usbserial-FT1Y6SYF"
+
 case $1 in
-  make)  docker run -i -v=/Users/taku/OS12step:/work -w=/work/src/$2 os12step make;;
-  image) docker run -i -v=/Users/taku/OS12step:/work -w=/work/src/$2 os12step make image;;
-  write) /Users/taku/OS12step/tools/kz_h8write -3069 -f20 /Users/taku/OS12step/src/$2/kzload.mot /dev/tty.usbserial-FT1Y6SYF;;
-  clean) docker run -i -v=/Users/taku/OS12step:/work -w=/work/src/$2 os12step make clean;;
+    make)
+        docker run -i -v=$WORKDIR:/work -w=/work/src/$2 os12step make
+        ;;
+
+    image)
+        docker run -i -v=$WORKDIR:/work -w=/work/src/$2 os12step make image
+        ;;
+
+    write)
+        $WORKDIR/tools/kz_h8write -3069 -f20 $WORKDIR/src/$2/kzload.mot $USBDEV
+        ;;
+
+    clean)
+        docker run -i -v=$WORKDIR:/work -w=/work/src/$2 os12step make clean
+        ;;
+
 esac
