d41e021d51947db5760750815b363ed4d20452e7
dondakeshimo <went.went.takkun135@gmail.com>
2018-06-17 19:07:45 +0900

    [add]ROMのデータをRAMにコピーして静的変数を変更可能にするinitを実装

--- src/03/bootload/main.c
+++ src/03/bootload/main.c
@@ -2,6 +2,24 @@
 #include "serial.h"
 #include "lib.h"
 
+static int init(void)
+{
+    /* 以下はリンカ・スクリプトで定義してあるシンボル */
+    extern int erodata, data_start, edata, bss_start, ebss;
+
+    /*
+     * データ領域とBSS領域を初期化する。この処理以降でないと、
+     * グローバル変数が初期化されていないので注意
+     */
+    memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
+    memcmp(&bss_start, 0, (long)&ebss - (long)&bss_start);
+
+    /* シリアルの初期化 */
+    serial_init(SERIAL_DEFAULT_DEVICE);
+
+    return 0;
+}
+
 int main(void)
 {
     serial_init(SERIAL_DEFAULT_DEVICE);
